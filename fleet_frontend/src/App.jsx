3ab1906e7ca6f7f2c621c38d3246bea50c3e3278
WojKarwinski <wojciech.karwinski@student.hogent.be>
2023-11-26 12:37:57 +0100

    sum

--- fleet_frontend/src/App.jsx
+++ fleet_frontend/src/App.jsx
@@ -1,21 +1,16 @@
 import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
 import DriverComponentBS from "./components/DriverComponentBS";
 import SearchDriverComponent from "./components/SearchDriverComponent";
-import AddNewDriverComponent from "./components/AddNewDriverComponent"; // Ensure this path is correct
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import AddDriverButtonComponent from "./components/AddDriverButtonComponent";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
-  const [showModal, setShowModal] = useState(false);
 
   const handleSearchChange = (newQuery) => {
     setSearchQuery(newQuery);
   };
-
-  const handleModalShow = () => setShowModal(true);
-  const handleModalClose = () => setShowModal(false);
 
   return (
     <>
@@ -23,17 +18,7 @@ function App() {
         searchQuery={searchQuery}
         onSearchChange={handleSearchChange}
       />
-      <Button variant="primary" onClick={handleModalShow}>
-        Add New Driver
-      </Button>
-      <Modal show={showModal} onHide={handleModalClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add New Driver</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <AddNewDriverComponent />
-        </Modal.Body>
-      </Modal>
+      <AddDriverButtonComponent />
       <DriverComponentBS searchQuery={searchQuery} />
     </>
   );
